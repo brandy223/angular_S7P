c18bd234d0e3cb66b9f16ed2ced3f1fb63888a8d
antho <anthony.volpelliere@gmail.com>
2023-12-08 15:16:04 +0100

    Search auto

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {FormControl, FormGroup, UntypedFormGroup, Validators} from "@angular/forms";
-import {map, Observable, startWith, switchMap} from "rxjs";
+import {debounceTime, filter, map, Observable, startWith, switchMap, tap} from "rxjs";
 import {RawgService} from "../rawg.service";
 import {JeuxComponent} from "../jeux/jeux.component";
 import {Jeu} from "../jeu.interface";
@@ -26,29 +26,18 @@ export class SearchComponent {
       })
     }
 
-    ngOnInit():void {
-      this.rawgService.getGamesToFilter().subscribe(
-          (data: any[]) => { console.log(data); this.games = data}
-      )
-
-      this.searchCtrl.valueChanges.pipe(
-          switchMap( (val: string) => this.rawgService.getGamesContains(val))
-      ).subscribe(
-          (games: Jeu[]) => this.games = games
-      )
+    @Output() eventOut = new EventEmitter<Jeu[]>()
+    ngOnInit(): void {
+        this.searchCtrl.valueChanges.pipe(
+            filter(searchValue => searchValue !== ''),
+            switchMap(searchValue => this.submit(searchValue))
+        ).subscribe(filteredGames => {
+            this.games = filteredGames;
+            this.eventOut.emit(this.games);
+        });
     }
 
-  @Output() eventOut = new EventEmitter<Jeu[]>()
-  submit() {
-    if (this.searchCtrl.value) {
-      const searchValue = this.searchCtrl.value.toLowerCase();
-      this.rawgService.getGamesToFilter(searchValue).pipe(
-          map((jeux: Jeu[]) => jeux.filter(jeu => jeu.name.toLowerCase().includes(searchValue)))
-      ).subscribe(filteredGames => {
-        this.games = filteredGames;
-        console.log(this.games)
-        this.eventOut.emit(this.games);
-      });
+    submit(searchValue: string): Observable<Jeu[]> {
+        return this.rawgService.getGamesToFilter(searchValue);
     }
-  }
 }
