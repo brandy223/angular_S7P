d2d80c6e8e72d930132b297fff80b763a294be40
Brandy223 <brandan.legall@gmail.com>
2023-12-08 15:52:43 +0100

    feat: get games detail in rawg service fix: removed "games" route from main url

--- src/app/rawg.service.ts
+++ src/app/rawg.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map, Observable} from 'rxjs';
-import {JeuxComponent} from "./jeux/jeux.component";
 import { Jeu } from './jeu.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RawgService {
-  private baseUrl: string = 'https://api.rawg.io/api/games';
+  private baseUrl: string = 'https://api.rawg.io/api';
   private apiKey: string = '60ed34d7548b41598ab2d6503daa4cb1';
 
   constructor(private http: HttpClient) { }
 
   getGames(page: number = 1, pageSize: number = 20): Observable<any> {
-    return this.http.get(`${this.baseUrl}?key=${this.apiKey}&page=${page}&page_size=${pageSize}`);
+    return this.http.get(`${this.baseUrl}/games?key=${this.apiKey}&page=${page}&page_size=${pageSize}`);
+  }
+
+  getGamedetails(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/games/${id}?key=${this.apiKey}`);
   }
 
   getGamesToFilter(search: string = '', page: number = 1, pageSize: number = 20): Observable<Jeu[]> {
-    return this.http.get<any>(`${this.baseUrl}?key=${this.apiKey}&search=${search}`).pipe(
+    return this.http.get<any>(`${this.baseUrl}/games?key=${this.apiKey}&search=${search}`).pipe(
         map(response => response.results)
     );
   }
